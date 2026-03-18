78b95280124530bb7b95ae87b301e4cc1739316f
Lukas-Bohez <lukas.bohez@student.howest.be>
2026-03-18 11:18:42 +0100

    Enhance launcher UI to allow specifying server host/port for server+client startup

--- launcher/launcher.c
+++ launcher/launcher.c
@@ -8,6 +8,8 @@
 static HWND hStartServer;
 static HWND hStartClient;
 static HWND hStartBoth;
+static HWND hServerHostEdit;
+static HWND hServerPortEdit;
 static HWND hStatus;
 static wchar_t g_basePath[MAX_PATH];
 
@@ -44,18 +46,48 @@ static void LaunchProcess(const wchar_t *path, const wchar_t *args)
     SetStatus(L"Started %s", path);
 }
 
+static void GetEditText(HWND hEdit, wchar_t *out, int outSize)
+{
+    GetWindowTextW(hEdit, out, outSize);
+}
+
 static void OnStartServer()
 {
     wchar_t serverPath[MAX_PATH];
     PathCombineW(serverPath, g_basePath, L"astonia_community_server3\\server.exe");
-    LaunchProcess(serverPath, L"");
+
+    wchar_t portStr[32] = L"";
+    GetEditText(hServerPortEdit, portStr, _countof(portStr));
+
+    wchar_t args[64] = L"";
+    if (portStr[0] != L'\0') {
+        swprintf_s(args, _countof(args), L"-p %s", portStr);
+    }
+
+    LaunchProcess(serverPath, args);
 }
 
 static void OnStartClient()
 {
     wchar_t clientPath[MAX_PATH];
     PathCombineW(clientPath, g_basePath, L"astonia_community_client\\bin\\moac.exe");
-    LaunchProcess(clientPath, L"");
+
+    wchar_t host[128] = L"";
+    GetEditText(hServerHostEdit, host, _countof(host));
+
+    wchar_t portStr[32] = L"";
+    GetEditText(hServerPortEdit, portStr, _countof(portStr));
+
+    wchar_t args[256] = L"";
+    if (host[0] != L'\0') {
+        swprintf_s(args, _countof(args), L"-d %s", host);
+    }
+    if (portStr[0] != L'\0') {
+        wcscat_s(args, _countof(args), L" -t ");
+        wcscat_s(args, _countof(args), portStr);
+    }
+
+    LaunchProcess(clientPath, args);
 }
 
 static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -63,14 +95,24 @@ static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPara
     switch (msg) {
     case WM_CREATE:
         {
+            CreateWindowW(L"STATIC", L"Server Host:", WS_CHILD | WS_VISIBLE | SS_LEFT,
+                10, 10, 80, 20, hwnd, NULL, NULL, NULL);
+            hServerHostEdit = CreateWindowW(L"EDIT", L"127.0.0.1", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+                90, 10, 140, 20, hwnd, NULL, NULL, NULL);
+
+            CreateWindowW(L"STATIC", L"Server Port:", WS_CHILD | WS_VISIBLE | SS_LEFT,
+                240, 10, 80, 20, hwnd, NULL, NULL, NULL);
+            hServerPortEdit = CreateWindowW(L"EDIT", L"5556", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+                320, 10, 70, 20, hwnd, NULL, NULL, NULL);
+
             hStartServer = CreateWindowW(L"BUTTON", L"Start Server", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                10, 10, 120, 30, hwnd, (HMENU)1, NULL, NULL);
+                10, 40, 120, 30, hwnd, (HMENU)1, NULL, NULL);
             hStartClient = CreateWindowW(L"BUTTON", L"Start Client", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                140, 10, 120, 30, hwnd, (HMENU)2, NULL, NULL);
+                140, 40, 120, 30, hwnd, (HMENU)2, NULL, NULL);
             hStartBoth = CreateWindowW(L"BUTTON", L"Start Both", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-                270, 10, 120, 30, hwnd, (HMENU)3, NULL, NULL);
+                270, 40, 120, 30, hwnd, (HMENU)3, NULL, NULL);
             hStatus = CreateWindowW(L"STATIC", L"Ready", WS_CHILD | WS_VISIBLE | SS_LEFT,
-                10, 60, 380, 25, hwnd, NULL, NULL, NULL);
+                10, 80, 380, 25, hwnd, NULL, NULL, NULL);
         }
         break;
     case WM_COMMAND:
@@ -114,7 +156,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     RegisterClassW(&wc);
 
     HWND hwnd = CreateWindowExW(0, CLASS_NAME, L"Astonia Launcher", WS_OVERLAPPEDWINDOW & ~WS_THICKFRAME,
-        CW_USEDEFAULT, CW_USEDEFAULT, 420, 140, NULL, NULL, hInstance, NULL);
+        CW_USEDEFAULT, CW_USEDEFAULT, 420, 180, NULL, NULL, hInstance, NULL);
 
     if (!hwnd) {
         return 0;
